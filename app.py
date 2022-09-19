87603aecee1c4dfb6a6fbdd6a16bd741140e1f95
asherorr <asher@mailfence.com>
2022-09-19 14:32:57 -0400

    This commit will add the templates folder and add index.html to the folder.

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello from Pet Adoption"
+    return render_template('index.html')
 
-@app.route('/order')
-def order():
-    return "Hello from order!"
-    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
