2340ac8c6520ca0fe4e59a25f20d12271865c2b5
asherorr <asher@mailfence.com>
2022-09-20 12:25:56 -0400

    This commit will add the add-pet and pet routes to app.py.

--- app.py
+++ app.py
@@ -7,6 +7,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/add-pet')
+def add_pet():
+    return render_template('addpet.html')
+
+@app.route('/pet')
+def pet():
+    return render_template('pet.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000, host='127.0.0.1')
