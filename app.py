60b0f1d59ceae32eae8da420b9bdbae692442ee3
asherorr <asher@mailfence.com>
2022-09-20 14:35:41 -0400

    This commit will add the GET and POST methods to the add_pet route.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, url_for
+from flask import (Flask, render_template, 
+                   url_for, request)
 
 
 app = Flask(__name__)
@@ -7,8 +8,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/add-pet')
+@app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
+    print(request.form)
+    print(request.form['name'])
     return render_template('addpet.html')
 
 @app.route('/pet')
