49d4da9a40db639f350d9c8daca3b975c918f15f
asherorr <asher@mailfence.com>
2022-09-22 15:07:31 -0400

    This commit will add the ability to update and delete entries in pets.db.

--- app.py
+++ app.py
@@ -3,12 +3,11 @@ from flask import (render_template, redirect,
 from models import db, Pet, app
 
 
-
-
 @app.route('/')
 def index():
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
+
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
@@ -27,11 +26,41 @@ def add_pet():
         return redirect(url_for('index'))
     return render_template('addpet.html')
 
+
 @app.route('/pet/<id>')
 def pet(id):
     pet = Pet.query.get(id)
     return render_template('pet.html', pet=pet)
 
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_pet(id):
+    pet = Pet.query.get(id)
+    if request.form:
+        pet.name = request.form['name']
+        pet.age = request.form['age']
+        pet.breed = request.form['breed']
+        pet.color = request.form['color']
+        pet.size = request.form['size']
+        pet.weight = request.form['weight']
+        pet.url = request.form['url']
+        pet.url_tag = request.form['alt']
+        pet.pet_type = request.form['pet']
+        pet.gender = request.form['gender']
+        pet.spay = request.form['spay']
+        pet.house_trained = request.form['housetrained']
+        pet.description = request.form['description']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('editpet.html', pet=pet)
+
+
+@app.route('/delete/<id>')
+def delete_pet(id):
+    pet = Pet.query.get(id)
+    db.session.delete(pet)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     db.create_all()
